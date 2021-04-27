f8f9aceb1ac25fcc5cf9b14aa22892bbd4c2b276
cht <3237126351@qq.com>
2021-04-27 18:43:20 +0800

    z

---
+++ Z/z.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+    public:
+        Solution(string astring){
+            this->astring=astring;
+        }
+        string convert(int numRows){
+            if (numRows==1) {
+                return astring;
+            }
+            string v[numRows];
+            for (int i = 0; i < astring.length(); ++i) {
+                int m = i%(2*numRows-2);
+                if (m<numRows) {
+                   v[m].push_back(astring[i]);
+                }else {
+                   m=2*numRows-m-2;
+                   for (int j = 0; j < m; ++j) {
+                       v[j].push_back(' ');
+                       std::cout << "test" << std::endl;
+                   }
+                   v[m].push_back(astring[i]);
+                   for (int j = m+1; j < numRows; ++j) {
+                       v[j].push_back(' ');
+                   }
+
+                }
+            }
+            for (int i = 0; i < numRows; ++i) {
+                std::cout << v[i] << std::endl;
+            }
+            string output;
+            for (int i= 0; i < numRows; ++i) {
+                output+=v[i];
+            }
+            return output;
+        }
+    private:
+        string astring;
+};
+int main()
+{
+    Solution *test = new Solution("abcdefghi");
+    std::cout << test->convert(2) << std::endl;
+}
