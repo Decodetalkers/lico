0c2eb7b4719de31200bb208cc30373f94d828a70
cht <3237126351@qq.com>
2021-07-28 12:09:02 +0800

    add list

---
+++ rust/bin/list.rs
@@ -0,0 +1,42 @@
+enum List {
+    Cons(u32,Box<List>),
+    Nil,
+}
+use List::*;
+impl List {
+    fn new() -> Self {
+        Nil
+    }
+
+    fn prepend(self, elem: u32) -> Self {
+        Cons(elem,Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_,ref tail) => 1+ tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{},{}",head,tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
+}
+fn main() {
+    let mut list = List::new();
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+    
+    println!("linked list has length:{}",list.len());
+    println!("{}",list.stringify());
+    
+}
